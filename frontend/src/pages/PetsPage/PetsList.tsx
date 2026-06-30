0bc5bf7a43e0d64f0a6d6c3eddc2fb75f5b008c7
Tatjana Klatyik <TatjanaKlatyik@aol.com>
2026-06-30 15:49:42 +0200

    fix frontend

--- frontend/src/pages/PetsPage/PetsList.tsx
+++ frontend/src/pages/PetsPage/PetsList.tsx
@@ -1,18 +1,23 @@
 import type { Pet } from "./types/Pet";
-import { Box, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { Box } from "@mui/material";
+import { DataGrid, type GridColDef } from "@mui/x-data-grid";
 
 export default function PetsTable({ pets }: { pets: Pet[] }) {
   const safePets = Array.isArray(pets) ? pets : [];
 
-  const rows = safePets.map((pet, index) => ({
-    id: pet.petId ?? index,
-    name: pet.petName ?? "No name",
-    type: pet.pet_type_id ?? "Unknown"
-  }));
+ const rows = safePets.map((pet: any, index) => ({
+  id: pet.pet_id ?? pet.petId ?? index,
+  name: pet.pet_name ?? pet.name ?? pet.petName ?? "No name",
+  type: pet.pet_type_id ?? pet.petTypeId ?? "Unknown",
+  birthDate: pet.birthDate ?? "Unknown"
 
-  const columns = [
-    { field: "name", headerName: "Name", width: 150 },
-    { field: "type", headerName: "Type", width: 150 }
+}));
+
+
+  const columns: GridColDef[] = [
+    { field: "name", headerName: "Name", width: 250 },
+    { field: "type", headerName: "Type", width: 100 },
+    { field: "birthDate", headerName: "Birth Date", width: 150 }
   ];
 
   console.log("pets:", pets);
